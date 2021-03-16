ef67e0f7de8d04eca5d6154251c8b73c166be92c
David634 <david6304murray@gmail.com>
2021-03-16 23:40:41 +0000

    python version finished but working on java still

--- Main.java
+++ Main.java
@@ -1,5 +1,21 @@
+//This doesn't work but I made a version in python that does and added it as a text file
+
+import java.io.*;
+import java.util.*;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
   }
+//Module to read data from file and store it in an array of records
+  public static Member[] readData() {
+
+  }
+}
+
+
+//Create record structure for members 
+class Member {
+  String forename;
+  String surname;
+  double distance;
 }
